f122f9295b0363fcfb7e840ee2b39aa344c91355
HaPhan Tran <47532906+haphantran@users.noreply.github.com>
2019-12-21 15:44:03 -0500

    Updates

---
+++ precache-manifest.3125d391ac247380a631664d20143017.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79136291b624a9038a619ca9a0649807",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a160e88b9667673c2ece",
+    "url": "/monsters-rolodex/static/css/main.02f91107.chunk.css"
+  },
+  {
+    "revision": "c728ba4a2366e885ec54",
+    "url": "/monsters-rolodex/static/js/2.d8d20882.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.d8d20882.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a160e88b9667673c2ece",
+    "url": "/monsters-rolodex/static/js/main.cbbaf915.chunk.js"
+  },
+  {
+    "revision": "dc25ba12107eb546721c",
+    "url": "/monsters-rolodex/static/js/runtime-main.75c8e44f.js"
+  }
+]);
